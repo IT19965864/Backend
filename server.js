2083a8251d05cbc59f2d5a7970dd5e52e18b7450
menaka809 <malithjayasekara2255721@gmail.com>
2022-03-06 21:30:55 +0530

    add database uri

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 8070;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(
+  MONGODB_URI,
+  {
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) {
+      console.log("Database Error:", error.message);
+    }
+  }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("Database Synced");
+});
+
+app.route("/").get((req, res) => {
+  res.send("malith");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on PORT ${PORT}`);
+});
